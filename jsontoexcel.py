9ea2da8f444ef2652640913ad40f9558dee3cf83
Merc <albingeov@gmail.com>
2021-03-03 17:13:02 +0530

    py corrected

---
+++ jsontoexcel.py
@@ -0,0 +1,22 @@
+import requests
+import tablib
+import json
+
+
+class Jsonexcel:
+
+    def convert(self):
+        request_url = 'https://5f717a7064a3720016e60748.mockapi.io/students/students'
+        data = requests.get(request_url)
+        json_string = json.dumps(json.loads(data.text))
+        # print(json_string)
+
+        ds = tablib.Dataset()
+        ds.json = json_string
+        # print(ds)
+        with open('jsontoexcel.xlsx', 'wb') as f:
+            f.write(ds.export('xlsx'))
+
+
+jsobj = Jsonexcel()
+jsobj.convert()
